42d10a3dca204f4aa050ebfafbcbb645b3b7e77d
eccenux <eccenux+git@gmail.com>
2019-09-30 23:42:28 +0200

    alfa version

---
+++ side-tool-selector.meta.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @id             iitc-plugin-side-tool-selector@eccenux
+// @name           IITC plugin: Side tools selector.
+// @category       Info
+// @version        0.0.1
+// @description    [0.0.1] Allows to choose which side tools should be temporarly hidden.
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
